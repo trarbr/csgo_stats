58458df29113b49ca41abcd8d429348dfa779928
Troels Brødsgaard <troels@uptime.dk>
2020-11-02 22:53:42 +0100

    Refactor frontend to follow convention, use live_redirect

--- mix.exs
+++ mix.exs
@@ -20,7 +20,7 @@ defmodule CsgoStats.MixProject do
   def application do
     [
       mod: {CsgoStats.Application, []},
-      extra_applications: [:logger, :runtime_tools]
+      extra_applications: [:logger, :runtime_tools, :inets]
     ]
   end
 
