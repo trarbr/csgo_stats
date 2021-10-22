d3d19d8782cb2b5b66c56602ef38297361560910
Troels Brødsgaard <troels@inilab.dk>
2021-10-22 13:54:04 +0200

    Update Erlang and Elixir versions and libraries

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule CsgoStats.MixProject do
     [
       app: :csgo_stats,
       version: "0.1.0",
-      elixir: "~> 1.5",
+      elixir: "~> 1.12",
       elixirc_paths: elixirc_paths(Mix.env()),
       compilers: [:phoenix, :gettext] ++ Mix.compilers(),
       start_permanent: Mix.env() == :prod,
@@ -33,18 +33,18 @@ defmodule CsgoStats.MixProject do
   # Type `mix help deps` for examples and options.
   defp deps do
     [
-      {:phoenix, "~> 1.5.0"},
+      {:phoenix, "~> 1.6.0"},
       {:phoenix_pubsub, "~> 2.0"},
       {:phoenix_ecto, "~> 4.0"},
       {:ecto_sql, "~> 3.1"},
       {:postgrex, ">= 0.0.0"},
-      {:phoenix_html, "~> 2.11"},
+      {:phoenix_html, "~> 3.0"},
       {:phoenix_live_reload, "~> 1.2", only: :dev},
       {:gettext, "~> 0.11"},
       {:jason, "~> 1.0"},
       {:plug_cowboy, "~> 2.1"},
       {:nimble_parsec, "~> 1.1"},
-      {:phoenix_live_view, "~> 0.15.5"},
+      {:phoenix_live_view, "~> 0.17.1"},
       {:benchee, "~> 1.0", only: :dev}
     ]
   end
